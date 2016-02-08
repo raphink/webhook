06fbabab8b05a93bae10053d528e784bab1b41c8
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-08 10:46:06 +0100

    Pass a HOOKS_SECRET environment variable
    This will replace all instances of %{HOOKS_SECRET}
    in the hooks json files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [ -n $HOOKS_URI ]; then
   git clone "$HOOKS_URI" /etc/webhook
+  sed -i "s/%{HOOKS_SECRET}/${HOOKS_SECRET}/g" /etc/webhook/*.json
 fi
 
 exec /go/bin/webhook -hooks /etc/webhook/*.json -verbose
